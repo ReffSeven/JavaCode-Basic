05a4265c32a01ce2719c0edb3cd597d1700061c0
ReffSeven <74449710+ReffSeven@users.noreply.github.com>
2020-11-19 01:23:01 +0700

    Create SegitigaBintang.java

---
+++ segitiga/SegitigaBintang.java
@@ -0,0 +1,11 @@
+public class SegitigaBintang {
+    public static void main(String[] args) {
+        int N = 5;
+        for(int y = -1; ++y < N;) {
+            for(int x = -1; ++x <= y;) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
